0c1370782fbe982283478064dc9204e7af98135e
reched <darius.rechesan@cpp.canon>
2022-05-13 16:51:34 +0300

    finished Form basics and Data binding

---
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
+
+@Component({
+  selector: 'app-user-settings-form',
+  templateUrl: './user-settings-form.component.html',
+  styleUrls: ['./user-settings-form.component.css']
+})
+export class UserSettingsFormComponent implements OnInit {
+
+  originalUserSettings:UserSettings={
+    name: "Andrei",
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: "Annual",
+    notes: "Some notes here..."
+  };
+  userSettings:UserSettings={ ...this.originalUserSettings};
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
